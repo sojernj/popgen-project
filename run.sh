3ac0929ea0a2a7f78a57736939a5dc47e747fc24
Søren Jørgensen <201906763@post.au.dk>
2024-06-05 15:52:14 +0200

    Removed the large file archaic_admixture.out

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=archaic_admixture
 #SBATCH --output=archaic_admixture.out
 #SBATCH --error=archaic_admixture.err
-#SBATCH --time=03:00:00
+#SBATCH --time=02:00:00
 #SBATCH -c 70
-#SBATCH --mem=10GB
+#SBATCH --mem-per-cpu=2GB
 
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate hpc_course_r
